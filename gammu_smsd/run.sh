a7c6cf18ff805c5ad141c56f22d7035284d3c4c4
rychlymartin82-debug <rychlymartin82@gmail.com>
2026-01-11 15:26:16 +0100

    run.sh

--- gammu_smsd/run.sh
+++ gammu_smsd/run.sh
@@ -1,13 +1,24 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 set -e
 
-CONFIG_DIR=/data
-SMSD_CONFIG=/etc/gammu-smsdrc
+bashio::log.info "Starting Gammu SMSD add-on..."
 
-echo "Starting Gammu SMSD..."
-echo "Using device: ${DEVICE}"
+# Load configuration
+DEVICE=$(bashio::config 'device')
+PIN=$(bashio::config 'pin')
+LOGLEVEL=$(bashio::config 'loglevel')
 
-cat <<EOF > $SMSD_CONFIG
+bashio::log.info "Using device: ${DEVICE}"
+bashio::log.info "Log level: ${LOGLEVEL}"
+
+CONFIG_DIR="/data"
+SMSD_CONFIG="/etc/gammu-smsdrc"
+
+# Create directories
+mkdir -p /data/inbox /data/outbox /data/sent /data/error
+
+# Generate SMSD config
+cat <<EOF > "${SMSD_CONFIG}"
 [gammu]
 device = ${DEVICE}
 connection = at
@@ -24,6 +35,8 @@ errorpath = /data/error/
 PIN = ${PIN}
 EOF
 
-mkdir -p /data/inbox /data/outbox /data/sent /data/error
+bashio::log.info "Configuration file created at ${SMSD_CONFIG}"
 
-exec gammu-smsd --config $SMSD_CONFIG --pid /data/smsd.pid
+# Start SMSD
+bashio::log.info "Launching Gammu SMSD..."
+exec gammu-smsd --config "${SMSD_CONFIG}" --pid /data/smsd.pid
